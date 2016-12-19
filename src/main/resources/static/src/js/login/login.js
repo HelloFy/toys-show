92d1776011172774357274cc442a9b7d905d38fb
Fei Yue <916073811@qq.com>
2016-12-19 18:18:15 +0800

    update

--- src/main/resources/static/src/js/login/login.js
+++ src/main/resources/static/src/js/login/login.js
@@ -1,12 +1,12 @@
-var Vue= require('vue');;;;;;;;;;;;;;;;;;;;;;;;;
-var Login=require('./login.vue');;;;;;;;;;;;;;;;;;;;;;;;;
-var Background = require('../components/background.vue');;;;;;;;;;;;;;;;;;;;;;;;;
-var Footer = require('../components/footer.vue');;;;;;;;;;;;;;;;;;;;;;;;;
-var VueRouter = require('vue-router');;;;;;;;;;;;;;;;;;;;;;;;;
-Vue.use(VueRouter);;;;;;;;;;;;;;;;;;;;;;;;;
+var Vue= require('vue');
+var Login = require('./login.vue');
+var Background = require('../components/background.vue');
+var Footer = require('../components/footer.vue');
+var VueRouter = require('vue-router');
+Vue.use(VueRouter);
 const router = new VueRouter(
     require('../../../routes.js')
-);;;;;;;;;;;;;;;;;;;;;;;;;
+);
 new Vue({
     el : '.ts',
     components :{
@@ -16,4 +16,4 @@ new Vue({
     },
     router:router,
     render: h => h(Login),
-});;;;;;;;;;;;;;;;;;;;;;;;;
+});
